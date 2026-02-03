1102f3cf39fe77729e6a06bcdef7c80063bc31f9
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-02-03 23:14:11 +0000

    Solve exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const ogString = string.slice()
+    if (num < 0) return "ERROR";
+    if (num === 0) return "";
+    for (let i = 0; i < num - 1; i++) {
+        string += ogString;
+    }
+    return string;
 };
 
 // Do not edit below this line
