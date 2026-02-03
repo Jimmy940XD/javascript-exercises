79a08633098ff7ea1660dec5b1d544b9b54b10b0
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-02-03 23:24:17 +0000

    Solve exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (!string) return string;
+    let arr = string.split("");
+    arr.reverse();
+    const newString = arr.reduce((str, char) => str + char);
+    return newString;
 };
 
 // Do not edit below this line
