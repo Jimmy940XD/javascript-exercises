0e9cd6cae4b4ea71603ccf540fbe053de2a49488
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-02-04 00:47:08 +0000

    Solve exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+"use strict";
+
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        array = array.filter(item => item !== arg);
+    }
+    return array;
 };
 
 // Do not edit below this line
