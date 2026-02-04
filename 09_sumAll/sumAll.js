9abd7d43e8126d19043d7f1384934cdc88701e16
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-02-04 01:48:58 +0000

    Solve exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    switch (false) {
+        case (Number.isInteger(a) && Number.isInteger(b)):
+        case (a >= 0 && b >= 0):
+            return "ERROR";
+    }
+    let arr = [a, b].sort((a, b) => a - b);
+    sum = 0;
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
