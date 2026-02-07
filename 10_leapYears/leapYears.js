df0e41bd9f8bd5df96bd82651696ddf5bfe3762b
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-02-07 16:20:32 +0000

    Solve exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const cond1 = year % 4 === 0;
+    const cond2 = year % 100 !== 0;
+    const cond3 = year % 400 === 0;
+    if (cond1 && (cond2 || cond3)) return true;
+    return false;
 };
 
 // Do not edit below this line
